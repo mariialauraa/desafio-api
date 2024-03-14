b0052721ccab2f65580d1a6384516a8533a987ca
mariialauraa <mlauraifg@gmail.com>
2024-03-14 14:36:50 -0300

    P2: Att do model OrderProduct

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -6,4 +6,14 @@ class OrderProduct < ApplicationRecord
   validates :product_id, presence: true
   validates :quantity, presence: true
   validates :box, inclusion: { in: [true, false] }
+
+  validate :check_box_uniqueness
+
+  private
+
+  def check_box_uniqueness
+    if OrderProduct.where(product_id: product_id, box: box).where.not(id: id).exists?
+      errors.add(:box, "Já existe um produto com o mesmo product_id e box definido como #{box ? 'true' : 'false'}")
+    end
+  end
 end
